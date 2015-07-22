9db8a58483fe977049fef1d20aba10a9126af2ec
dblairchappell <d_blair_chappell@yahoo.co.uk>
2015-07-22 23:42:50 +0100

    working opening and save in GUI

--- prigogine/gui/PrigogineGUI.py
+++ prigogine/gui/PrigogineGUI.py
@@ -2,6 +2,7 @@
 from PyQt4 import QtCore, uic
 from PyQt4.QtGui import *
 import sys
+import json
 
 form_class = uic.loadUiType("Prigogine.ui")[0]
 
@@ -11,6 +12,7 @@ class MyWindowClass(QMainWindow, form_class):
 
     def __init__(self, parent=None):
 
+        self.workingData = {"populationCode": "", "simulationScript": ""}
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
         self.showMaximized()
@@ -36,8 +38,7 @@ class MyWindowClass(QMainWindow, form_class):
 
     def showNewFileDialog(self):
         fileDialog = QFileDialog()
-        fileName = fileDialog.getSaveFileName(self, 'New File', '/home/','.pr')
-        print fileName
+        fileName = fileDialog.getSaveFileName(self, 'New File', '/home/','*.pr')
         with open(fileName, 'w') as outfile:
            with open(fileName) as infile:
                 for line in infile:
@@ -46,13 +47,16 @@ class MyWindowClass(QMainWindow, form_class):
     #######################################
 
     def openFileButton_Clicked(self):
-        self.showNewFileDialog()
+        self.showOpenFileDialog()
 
     def showOpenFileDialog(self):
+
         fileDialog = QFileDialog()
-        fname = QFileDialog.getOpenFileName(fileDialog, 'Open Project', '/home/')
-        f = open(fname, 'r')
-        print f.read()
+        filename = QFileDialog.getOpenFileName(fileDialog, 'Open Project', '/home/','*.pr')
+        with open(filename, 'r') as inData:
+            data = json.load(inData)
+            self.modelCodeTextEdit.insertPlainText(data["populationCode"])
+            self.simulationScriptTextEdit.insertPlainText(data["simulationScript"])
 
     #######################################
 
@@ -60,12 +64,18 @@ class MyWindowClass(QMainWindow, form_class):
         self.showSaveAsDialog()
 
     def showSaveAsDialog(self):
+
         fileDialog = QFileDialog()
-        filename = QFileDialog.getOpenFileName(fileDialog, 'Open Project', '/home/')
-        dataToSave = "this is a test to see if data is saved"
+        filename = QFileDialog.getOpenFileName(fileDialog, 'Open Project', '/home/','*.pr')
+
+        popCode = str(self.modelCodeTextEdit.toPlainText())
+        self.workingData["populationCode"] = popCode
+
+        simCode = str(self.simulationScriptTextEdit.toPlainText())
+        self.workingData["simulationScript"] = simCode
+
         with open(filename, 'w') as outfile:
-            with open(filename) as infile:
-                outfile.write(dataToSave)
+            json.dump(self.workingData, outfile)
 
     #######################################
 
